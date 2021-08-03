6cf7965af7798e6edbc5888de59b2bb0dd805909
Victor Lucas <victor.lucas@tream-france.fr>
2021-08-03 10:47:23 +0200

    Update : 03-08-2021 10:47:23

--- deploy.sh
+++ deploy.sh
@@ -2,22 +2,21 @@
 
 NOW=$(date +"%d-%m-%Y %T")
 
-echo -e "\e[1;35mBuild site\e[0m"
+echo "Build site"
 
-cd blog
 hugo > /dev/null 2>&1
 
-echo -e "\e[1;35mCommit to the blog repository...\e[0m"
+echo -e "Commit to the blog repository..."
 
 git add .
 git commit -m "Update : $NOW" > /dev/null 2>&1
 git push origin main > /dev/null 2>&1
 
-echo -e "\e[1;35mCommit to the github.io repository...\e[0m"
+echo -e "Commit to the github.io repository..."
 
 cd public
 git add .
 git commit -m "Update : $NOW" > /dev/null 2>&1
 git push origin main > /dev/null 2>&1
 
-echo -e "\e[1;35mSuccessfully committed\e[0m"
+echo -e "Successfully committed"
